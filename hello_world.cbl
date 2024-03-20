f64e3f68a6d473c22b9d51d2b684438463f0ab19
Celso <161392506+Celsozne@users.noreply.github.com>
2024-03-20 05:49:42 +0000

    Start of study cobol

---
+++ hello_world.cbl
@@ -0,0 +1,16 @@
+      *Divisao de identificacao de variaveis(Divisao)
+       Identification Division.
+      * Nome do Programa(Secao)
+       Program-Id. hello_world.
+      *Declaracao de Variaveis(Paragrafo)
+      *Sentencas
+      *Statements
+      *characters
+       Data Division.
+       Working-Storage Section.
+
+
+       Procedure Division.
+           
+           GoBack.
+       End Program hello_world.      
